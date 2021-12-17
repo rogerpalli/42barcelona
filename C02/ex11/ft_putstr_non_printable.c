df9bbc9f6c5f41b9f1e0bb3d57b05df7813d3a81
Roger Lopez palli <rolopez-@cbr10s6.42barcelona.com>
2021-12-17 11:39:23 +0100

    backup

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,11 +6,10 @@
 /*   By: rolopez- <rolopez-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 12:47:40 by rolopez-          #+#    #+#             */
-/*   Updated: 2021/12/16 19:20:22 by rolopez-         ###   ########.fr       */
+/*   Updated: 2021/12/17 11:18:28 by rolopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -57,7 +56,7 @@ int	ft_str_is_printable(char *str)
 		}
 		else
 		{
-			putchar(str[i]);
+			ft_putchar(str[i]);
 		}
 		i++;
 	}
