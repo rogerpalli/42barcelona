72b3d4eff7cb0899bfd46f2136ed383fb85555c9
Roger Lopez palli <rolopez-@car9s1.42barcelona.com>
2021-12-20 10:49:28 +0100

    backup

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,30 +6,36 @@
 /*   By: rolopez- <rolopez-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 12:47:40 by rolopez-          #+#    #+#             */
-/*   Updated: 2021/12/17 14:10:50 by rolopez-         ###   ########.fr       */
+/*   Updated: 2021/12/20 10:40:21 by rolopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-char	*ft_strcapitalize(char *str)
+char	*ft_lowercase(char *str)
 {
-	unsigned int	i;
-	int				cap;
+	int	i;
 
 	i = 0;
-	cap = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
 			str[i] = str[i] + 32;
-		}
 		i++;
 	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	cap;
+
+	cap = 0;
+	ft_lowercase(str);
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
@@ -40,13 +46,9 @@ char	*ft_strcapitalize(char *str)
 			}
 		}
 		else if (str[i] >= '0' && str[i] <= '9')
-		{
 			cap = 1;
-		}
 		else
-		{
 			cap = 0;
-		}
 		i++;
 	}
 	return (str);
